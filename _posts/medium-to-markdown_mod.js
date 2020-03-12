378b0ff8d60777e1a5c8a00afc6b80841377f656
shekodn <sdiazn19@gmail.com>
2020-03-12 13:38:16 -0600

    Add why-infrastructure-as-code-is-lit

--- _posts/medium-to-markdown_mod.js
+++ _posts/medium-to-markdown_mod.js
@@ -1,5 +1,5 @@
 const mediumToMarkdown = require('medium-to-markdown');
 
-mediumToMarkdown.convertFromUrl("https://medium.com/insiden26/what-it-was-like-to-join-n26-abad7b072e25").then(function (markdown) {
+mediumToMarkdown.convertFromUrl("https://medium.com/@sergiodn/why-infrastructure-as-code-is-lit-bf95e08a7d89").then(function (markdown) {
   console.log(markdown); //=> Markdown content of medium post
 });
